51a2ec9c15cbc74545c9e90dce2614fd74677bda
chrispri2020 <chrispri@rams.colostate.edu>
2022-05-07 21:48:43 +0000

    My Eighth Project

--- MySeventhProject/App copy.js
+++ MySeventhProject/App copy.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App(){
+return(
+<View style={styles.container}>
+
+  <View style={styles.containerButton}>
+   <Button 
+      onPress={() => {
+        alert('You tapped button 1');
+     }}
+     title='Press Button 1'
+    />
+  </View>
+
+  <View style={styles.containerLayoutButton}>
+    <Button 
+      onPress={() => {
+        alert('You tapped button 2');
+      }}
+     title="Press Button 2"
+    />
+
+    <Button 
+      onPress={() => {
+        alert('You tapped button Great!');
+     }}
+     title="Great!"
+     color="green"
+    />
+
+  </View>
+
+  <View style={styles.containerButton}>
+    <Button 
+      onPress={() => {
+        alert('You tapped button Tap me!');
+     }}
+     title="Tap me!"
+    />
+  </View>
+
+
+</View>
+);
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  justifyContent: 'center',
+ },
+
+ containerButton: {
+   margin: 20,
+},
+
+containerLayoutButton: {
+  margin: 20,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+}
 });
+;
--- MySeventhProject/App.js
+++ MySeventhProject/App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default  class TouchableButton extends Component{
+
+onPressButton(){
+alert('You tapped a button')
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+render(){
+  return(
+    <View style={styles.container}>
+    
+      <View style={styles.containerButton}>
+       <Button 
+          onPress={this._onPressButton}
+         title='Press Button 1'
+        />
+      </View>
+    
+      <View style={styles.containerLayoutButton}>
+        <Button 
+          onPress={this._onPressButton}
+         title="Press Button 2"
+        />
+    
+        <Button 
+          onPress={this._onPressButton}
+         title="Great!"
+         color='green'
+        />
+    
+      </View>
+    
+      <View style={styles.containerButton}>
+        <Button 
+         onPress={this._onPressButton}
+         title="Tap me!"
+        />
+      </View>
+    
+    
+        </View>
+      );
+    }
+  } 
+    
+    const styles = StyleSheet.create({
+     container: {
+      flex: 1,
+      justifyContent: 'center',
+     },
+    
+     containerButton: {
+       margin: 20,
+    },
+    
+    containerLayoutButton: {
+      margin: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    }
+    });
